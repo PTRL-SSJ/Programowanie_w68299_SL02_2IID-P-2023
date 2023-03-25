c4096aa7d773818bb24d2def7320339ebd86e2fc
PRIDk <pepedawidowski@gmail.com>
2023-03-25 03:04:26 +0100

    Zadanie4 changes
    Completed Zadanie 4

--- Lab3/Zadanie4.js
+++ Lab3/Zadanie4.js
@@ -1,10 +1,17 @@
-window.onload = function Data() {
-    let d = new Date().toLocaleTimeString();
+// window.onload = function Data() {
+//     let d = new Date().toLocaleTimeString();
+//     document.getElementById("Data").innerHTML = d;
+// }
+
+// setTimeout(function() {
+//     let d = new Date().toLocaleTimeString();
+//     document.getElementById("Data").innerHTML = d;
+//     window.location.reload(1);
+//  }, 1000);
+
+function pokazDate() {
+    var d = new Date().toLocaleTimeString();
     document.getElementById("Data").innerHTML = d;
 }
 
-setTimeout(function() {
-    let d = new Date().toLocaleTimeString();
-    document.getElementById("Data").innerHTML = d;
-    window.location.reload(1);
- }, 1000);
+setInterval(pokazDate, 1000) // setTimeout does this once, after a second it reloads site and then displays the time. setInterval() does this every second till halt
